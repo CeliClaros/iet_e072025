047b1c5785f4d054e535c29122dfd5017efba7b4
CeliClaros <celinarclaros@gmail.com>
2025-07-12 20:50:16 -0300

    Add files via upload

---
+++ models/bd_conexion.php
@@ -0,0 +1,5 @@
+<?php
+
+	$conn = new mysqli('localhost', 'celina_dba', 'GxDm7aR1gCCq', 'celina_iet');
+	
+	?>
---
+++ models/registro.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once 'bd_conexion.php';
+
+
+//include_once 'includes/templates/header-barra.php';
+
+
+?>
